aa92ebae9c68cb882196cc6dc813d91836b91bb5
yujinyang1998 <yujinyang1998@users.noreply.github.com>
2020-09-18 17:43:13 +0800

    lambda2

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -8,6 +8,8 @@ import se.edu.inclass.task.TaskNameComparator;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.toList;
+
 public class Main {
 
     private TaskNameComparator taskNameComparator;
@@ -22,6 +24,9 @@ public class Main {
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
 
         printDeadlinesUsingStreams(tasksData);
+        for(Task t:filterByString(tasksData,"11")){
+            System.out.println(t);
+        }
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
@@ -48,10 +53,17 @@ public class Main {
         }
     }
 
-    public static void printDeadlinesUsingStreams(ArrayList<Task> tasksData) {
+    public static void printDeadlinesUsingStreams(ArrayList<Task> tasksData){
         tasksData.stream()
-                .filter((s) -> s instanceof Deadline)
-                .sorted((a, b) -> a.getDescription().toLowerCase().compareTo(b.getDescription().toLowerCase()))
+                .filter((s)->s instanceof Deadline)
+                .sorted((a,b)->a.getDescription().toLowerCase().compareTo(b.getDescription().toLowerCase()))
                 .forEach(System.out::println);
+    }
+
+    public static ArrayList<Task> filterByString(ArrayList<Task> tasksData, String filterString){
+        ArrayList<Task> filteredTaskList = (ArrayList<Task>) tasksData.stream()
+                .filter((s)->s.getDescription().contains(filterString))
+                .collect(toList());
+        return filteredTaskList;
     }
 }
